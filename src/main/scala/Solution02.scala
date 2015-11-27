f67a2a7a11569ebe6fd50006a69881c4dfcec328
tomerb <tomerb@liveperson.com>
2015-11-27 10:28:53 +0200

    +solution 2

--- src/main/scala/Solution02.scala
+++ src/main/scala/Solution02.scala
@@ -1,5 +1,7 @@
 import org.scalatest.{FlatSpec, ShouldMatchers}
 
+import scala.util.{Success, Try, Failure}
+
 /**
  * @author tomerb
  * on 26/11/15
@@ -7,8 +9,15 @@ import org.scalatest.{FlatSpec, ShouldMatchers}
 //noinspection ScalaStyle
 class Solution02 extends FlatSpec with ShouldMatchers {
   "the solution" should "find the last element of a list" in {
-    penultimate(List(1, 1, 2, 3, 5, 8)) should be (5)
+    penultimate(List(1, 1, 2, 3, 5, 8)) should be (Success(5))
+    penultimateRecur(List(1, 1, 2, 3, 5, 8)) should be (Success(5))
   }
 
-  def penultimate(input: Seq[Int]): Int = input.init.last
+  def penultimate(input: Seq[Int]): Try[Int] = Success(input.init.last)
+
+  def penultimateRecur(input: Seq[Int]): Try[Int] = input match {
+    case prevTail :: tail :: Nil => Success(prevTail)
+    case head :: tail => penultimateRecur(tail)
+    case _ => Failure(new NoSuchElementException("No such element")) // how to avoid inferred type containing nothing.
+  }
 }
