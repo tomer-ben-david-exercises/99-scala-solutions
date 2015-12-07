009a277129716c7329b5f4b1c38d06763d04bd35
tomerb <tomerb@liveperson.com>
2015-12-07 11:28:02 +0200

    +Solution8 with foldRight

--- src/main/scala/Solution08.scala
+++ src/main/scala/Solution08.scala
@@ -9,13 +9,18 @@ class Solution08 extends FlatSpec with ShouldMatchers {
   "compress" should "Eliminate consecutive duplicates of list elements" in {
     val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
 
-    compress(list) should be (List('a, 'b, 'c, 'a, 'd, 'e))
+    compressRecur(list) should be (List('a, 'b, 'c, 'a, 'd, 'e))
+    compressFold(list) should be (List('a, 'b, 'c, 'a, 'd, 'e))
   }
 
-  def compress(ls: List[Symbol]): List[Symbol] = ls match {
+  def compressFold(ls: List[Symbol]): List[Symbol] = ls.foldRight(List[Symbol]())((item,list) => {
+    if (list.isEmpty || list.head != item) item :: list
+    else list
+  })
+  def compressRecur(ls: List[Symbol]): List[Symbol] = ls match {
     case Nil => Nil
     case headItem :: Nil => List(headItem)
-    case item1 :: item2 :: tail if item1 == item2 => compress(item2 :: tail)
-    case item1 :: item2 :: tail => item1 :: compress(item2 :: tail)
+    case item1 :: item2 :: tail if item1 == item2 => compressRecur(item2 :: tail)
+    case item1 :: item2 :: tail => item1 :: compressRecur(item2 :: tail)
   }
 }
