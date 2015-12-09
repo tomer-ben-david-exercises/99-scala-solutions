f09e48b3a4051680d365efbaa808aeb81903d90e
tomerb <tomerb@liveperson.com>
2015-12-09 09:08:56 +0200

    +flatten with flatMap

--- src/main/scala/Solution07.scala
+++ src/main/scala/Solution07.scala
@@ -10,8 +10,23 @@ class Solution07 extends FlatSpec with ShouldMatchers {
     val list1: List[Any] = List(List(1, 1), 2, List(3, List(5, 8)))
     val list1Flattened = List(1, 1, 2, 3, 5, 8)
 
+//    flattenWithMap(list1) should be (list1Flattened)
+    flattenWithFlatmap(list1) should be (list1Flattened)
     flattenRecur(list1) should be (list1Flattened)
   }
+
+  def flattenWithFlatmap(ls: List[Any]): List[Any] = ls flatMap {
+    case internalList: List[_] => flattenWithFlatmap(internalList)
+    case element => List(element)
+  }
+
+//  def flattenWithMap(ls: List[Any]): List[Any] = ls map {
+//    case internalList: List[_] =>
+//      flattenWithMap(internalList)
+//    case element =>
+//      println (s"returning $element")
+//      element
+//  }
 
   def flattenRecur(ls: List[Any]): List[Any] = ls match {
     case Nil => Nil
