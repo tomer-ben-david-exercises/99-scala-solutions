d8ec831e47e3abf6456689fbb919ed47fac0b489
tomerb <tomerb@liveperson.com>
2015-12-31 16:18:58 +0200

    +Solution 10 encoder

---
+++ src/main/scala/Solution10.scala
@@ -0,0 +1,29 @@
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+/**
+ * @author tomerb
+ * on 26/11/15
+ */
+//noinspection ScalaStyle
+class Solution10 extends FlatSpec with ShouldMatchers {
+  "encode" should "Run-length encoding of a list" in {
+    val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    val encoded = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+
+    encode(list) should be (encoded)
+  }
+
+  def encode(ls: List[_]) = {
+    def encodeInternal(encodedSoFar: List[_], leftToEncode: List[_]): List[_] = {
+      leftToEncode match {
+        case Nil => encodedSoFar
+        case _ =>
+          val (similarToHead, rest) = leftToEncode.span(_ == leftToEncode.head)
+          val encodedSoFarWithNew = encodedSoFar :+ (similarToHead.size, similarToHead.head)
+          encodeInternal(encodedSoFarWithNew, rest)
+      }
+    }
+    encodeInternal(Nil, ls)
+  }
+
+}
